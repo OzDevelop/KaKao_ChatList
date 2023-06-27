b725f76d56247bb2ed623b69aff5f01ef5e120b9
byunsp3610@gmail.com <byunsp3610@gmail.com>
2023-06-27 22:31:25 +0900

    kakaochatlist_SwiftUI

---
+++ kakaoChatListView/kakaoChatListView/kakaoChatListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  kakaoChatListViewApp.swift
+//  kakaoChatListView
+//
+//  Created by 변상필 on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct kakaoChatListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
